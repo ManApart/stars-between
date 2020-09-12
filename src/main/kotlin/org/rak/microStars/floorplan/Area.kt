bffdb6530ff5f737a751b16766d195febf26bff9
ManApart <raustinkucera@gmail.com>
2020-09-11 22:58:58 -0400

    wip area groups

--- src/main/kotlin/org/rak/microStars/floorplan/Area.kt
+++ src/main/kotlin/org/rak/microStars/floorplan/Area.kt
@@ -1,17 +1,12 @@
 package org.rak.microStars.floorplan
 
-import org.rak.microStars.tile.DEFAULT_TILE
 import org.rak.microStars.tile.Tile
 
-class Area(val parent: FloorPlan, val isValidForDistance: (Tile) -> Boolean) {
+class Area(val parent: FloorPlan, val tiles: List<Tile>, val isValidForDistance: (Tile) -> Boolean) {
 
-
-    fun getTiles(): List<Tile> {
-        return listOf()
-    }
 
     fun getNeighbors(tile: Tile): List<Tile> {
-        return parent.getNeighbors(tile).filter { getTiles().contains(it) }
+        return parent.getNeighbors(tile).filter { tiles.contains(it) }
     }
 
 }
--- src/main/kotlin/org/rak/microStars/floorplan/AreaGroup.kt
+++ src/main/kotlin/org/rak/microStars/floorplan/AreaGroup.kt
@@ -2,15 +2,41 @@ package org.rak.microStars.floorplan
 
 import org.rak.microStars.tile.Tile
 
-class AreaGroup(val parent: FloorPlan, val isValidForDistance: (Tile) -> Boolean) {
+class AreaGroup(private val parent: FloorPlan, private val isValidForDistance: (Tile) -> Boolean) {
+    val areas = calculateAreas(parent, isValidForDistance)
 
-    //creates list of Areas when floorplan changes
-    fun calculateAreas(){
+    private fun calculateAreas(parent: FloorPlan, isValidForDistance: (Tile) -> Boolean) : List<Area>{
+        val areas = mutableListOf<Area>()
+        val tiles = parent.getAllTiles().filter { isValidForDistance(it) }.toMutableList()
 
+        while (tiles.isNotEmpty()){
+            val areaTiles = createAreaTiles(tiles)
+            areas.add(Area(parent, areaTiles, isValidForDistance))
+            tiles.removeAll(areaTiles)
+        }
+
+        return areas
     }
 
-    fun getAreas() : List<Area>{
-        return listOf()
+    private fun createAreaTiles(candidates: List<Tile>): List<Tile> {
+        val source = candidates.first()
+        val connected = mutableListOf(source)
+
+        val open = mutableListOf(source)
+        val closed = mutableListOf<Tile>()
+
+        while (open.isNotEmpty()){
+            val current = open.first()
+            open.remove(current)
+            if (!closed.contains(current)){
+                connected.add(current)
+                val neighbors = parent.getNeighbors(current).filter { isValidForDistance(it) }
+                open.addAll(neighbors)
+            }
+            closed.add(current)
+        }
+
+        return connected
     }
 
 }
