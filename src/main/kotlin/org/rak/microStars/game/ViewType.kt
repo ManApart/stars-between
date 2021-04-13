ef90984997fa81444bd8e5ed1740ffc8fc2a06bc
ManApart <raustinkucera@tutamail.com>
2021-04-12 20:09:01 -0400

    wip build panel only on overview

--- src/main/kotlin/org/rak/microStars/game/ViewType.kt
+++ src/main/kotlin/org/rak/microStars/game/ViewType.kt
@@ -4,11 +4,12 @@ import org.rak.microStars.views.View
 import org.rak.microStars.views.overview.SimpleFloorPlan
 import org.rak.microStars.views.powerView.PowerView
 import org.rak.microStars.views.airView.Airview
+import org.rak.microStars.views.shieldView.ShieldView
 
 enum class ViewType(val getView: () -> View) {
     OVERVIEW({ SimpleFloorPlan(Game.floorPlan) }),
     AIR({ Airview(Game.floorPlan) }),
     POWER({ PowerView(Game.floorPlan) }),
-    SHIELDS({ PowerView(Game.floorPlan) });
+    SHIELDS({ ShieldView(Game.floorPlan) });
 
 }
