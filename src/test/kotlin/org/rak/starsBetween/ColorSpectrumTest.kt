28f57e3535caa31669ae4b530d804defe550d1a8
ManApart <r@austinkucera.com>
2021-07-10 11:29:40 -0400

    update tests

--- src/test/kotlin/org/rak/starsBetween/ColorSpectrumTest.kt
+++ src/test/kotlin/org/rak/starsBetween/ColorSpectrumTest.kt
@@ -11,9 +11,9 @@ class ColorSpectrumTest {
     fun getNext() {
         val spectrum = ColorSpectrum(listOf(0 to Color.black, 50 to Color.blue, 100 to Color.white).toMap())
 
-        assertEquals(50, spectrum.getNext(0))
+        assertEquals(50, spectrum.getNext(1))
         assertEquals(50, spectrum.getNext(10))
-        assertEquals(100, spectrum.getNext(50))
+        assertEquals(100, spectrum.getNext(51))
         assertEquals(100, spectrum.getNext(100))
         assertEquals(100, spectrum.getNext(200))
     }
@@ -23,8 +23,8 @@ class ColorSpectrumTest {
         val spectrum = ColorSpectrum(listOf(0 to Color.black, 50 to Color.blue, 100 to Color.white).toMap())
 
         assertEquals(100, spectrum.getPrevious(200))
-        assertEquals(50, spectrum.getPrevious(100))
-        assertEquals(0, spectrum.getPrevious(50))
+        assertEquals(50, spectrum.getPrevious(99))
+        assertEquals(0, spectrum.getPrevious(49))
         assertEquals(0, spectrum.getPrevious(10))
         assertEquals(0, spectrum.getPrevious(0))
     }
