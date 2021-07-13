5549b37008e513dad54388b5966e3736aa98f6d8
ManApart <r@austinkucera.com>
2021-07-13 19:09:01 -0400

    wip variants of controls

--- src/commonMain/kotlin/ui/planetScene/PlanetControls.kt
+++ src/commonMain/kotlin/ui/planetScene/PlanetControls.kt
@@ -1,6 +1,11 @@
 package ui.planetScene
 
+import com.soywiz.korge.input.onClick
+import com.soywiz.korge.ui.uiButton
+import com.soywiz.korge.ui.uiComboBox
+import com.soywiz.korge.ui.uiVerticalStack
 import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.position
 import com.soywiz.korge.view.roundRect
 import com.soywiz.korim.color.Colors
 import planet.Planet
@@ -10,10 +15,26 @@ import ui.planetScene.planetView.PlanetViewType
 fun Container.createControls() {
 //    button(PlanetViewType.ALTITUDE.name)
     var offset = 10.0
-    PlanetViewType.values().forEach { view ->
-        roundRect(150.0, 40.0, 1.0, fill = Colors.BEIGE) {
-            y = offset
-        }
-        offset += 45
+
+    uiComboBox(items = PlanetViewType.values().toList()).onSelectionUpdate {
+        println(it.selectedItem)
     }
+
+//    uiVerticalStack(width = 150.0) {
+//        PlanetViewType.values().forEach { view ->
+//            uiButton(text = view.name) {
+//                onClick {
+//                    println("Clicked ${view.name}")
+//                }
+//            }
+//        }
+//    }.position(0.0, 32.0)
+
+
+//    PlanetViewType.values().forEach { view ->
+//        roundRect(150.0, 40.0, 1.0, fill = Colors.BEIGE) {
+//            y = offset
+//        }
+//        offset += 45
+//    }
 }
